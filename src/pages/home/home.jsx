2458591bf5bdbb4c28f28b82a86c247401b312f6
Anushervon777 <anushervonhalimov416@gmail.com>
2024-11-23 13:27:50 +0500

    main

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,270 @@
-export default function Home(){
+export default function Home() {
   return <>
-    <h1> home</h1>
+    <div className="" style={{ background: "radial-gradient(83.15% 154.4% at 10% 13.52%, #1E212C 0%, #232441 56.38%, #1E212C 100%)" }} >
+
+      <div className="flex items-start p-20">
+        <div className="w-1/2">
+          <h1 className="text-white text-5xl">Best <a className="text-purple-500">SEO & Marketing</a> Solutions for You</h1>
+          <div className="flex items-center">
+            <button className="border-white border-solid" style={{ border: "1px solid white", padding: "16px", color: "white" }}>Get a free analysis</button>
+            <img src="/src/pages/home/homeImg/play-video.png" alt="" />
+          </div>
+        </div>
+        <img src="/src/pages/home/homeImg/illustration (14).png" alt="" />
+      </div>
+      <img src="/src/pages/home/homeImg/features (2).png" className="text-center mx-28   w-3/4" alt="" />
+    </div>
+    <div className="flex p-28 gap-20 justify-center	" >
+      <p className="w-2/5 font-sans font-semibold text-3xl">Createx SEO Agency is a full-service digital marketing agency. We help businesses make more money through a wealth of performance data and market research. We create science-based SEO strategies to empower our clients.</p>
+      <div>
+        <div className="flex pb-10">
+          <div className="w-1/3">
+            <img src="/src/pages/home/homeImg/image (19).png" alt="" />
+            <div>
+              <p>Top SEO Companies 2020 by Clutch</p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <img src="/src/pages/home/homeImg/image (19).png" alt="" />
+            <div>
+              <p>Top SEO Companies 2020 by Clutch</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="w-1/3">
+            <img src="/src/pages/home/homeImg/image (19).png" alt="" />
+            <div>
+              <p>Top SEO Companies 2020 by Clutch</p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <img src="/src/pages/home/homeImg/image (19).png" alt="" />
+            <div>
+              <p>Top SEO Companies 2020 by Clutch</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="flex items-center	justify-center gap-20">
+      <div className="w-64 text-sm">
+        <h1 className="text-black font-semibold	  text-3xl	">Createx Agency </h1>
+        <p className=" text-gray-400 text-justify	">Lacus ac mollis vitae viverra. Tincidunt est, feugiat montes, viverra ullamcorper mi dui, nulla pharetra. Sapien urna nullam elit diam mauris orci. Volutpat quam tempus justo, aliquam. Tortor ultrices mattis eget orci eu nisi sed augue odio. Et senectus risus, pharetra, tristique libero. Dolor risus ac quam dictum mattis ultrices laoreet sapien. Feugiat lacus, suscipit eu sociis et quis hendrerit. Cras at ac magna ultricies orci.</p>
+        <button style={{ color: "white", background: "#7772F1", padding: "10px", borderRadius: "5px" }} >More Abouts us</button>
+      </div>
+      <img src="/src/pages/home/homeImg/image (20).png" alt="" />
+    </div>
+    <img className="w-1/2 justify-center  m-auto mt-28 mb-28" src="/src/pages/home/homeImg/Figures (2).png" alt="" />
+    <div>
+      <h1 className="text-center font-sans text-4xl">Our services</h1>
+      <div className="flex justify-center gap-10">
+        <p className="p-12">Social Media</p>
+        <p className="p-12">SEO</p>
+        <p className="p-12">Research</p>
+        <p className="p-12 ">Content & PR</p>
+        <p className="p-12 ">Payed Traffic</p>
+
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="text-start w-2/5">
+          <h1 className="font-semibold text-3xl">Complete Worflow For Any SEO Professional</h1>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <p>✅ Aenean enim tellus morbi nisl vulputate dictumst.</p>
+          <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+            <button style={{ color: "#7772F1", borderColor: "#7772F1", padding: "10px" }}>Learn more</button>
+            <button style={{ background: "#7772F1", padding: "10px", color: "white", borderRadius: "7px" }}>Try SEO toolkit</button>
+
+          </div>
+        </div>
+        <img src="/src/pages/home/homeImg/illustration (15).png" alt="" />
+      </div>
+
+    </div>
+    <div className="bg-gray-900 flex text-gray-400 items-center justify-center gap-24 p-16" >
+      <img src="/src/pages/home/homeImg/illustration (16).png" alt="" />
+      <div className="">
+        <h1>Get a Free SEO Analysis!</h1>
+        <div><input type="text" style={{ padding: "5px", margin: "10px", background: "#FFFFFF1F" }} name="" id="" /><input type="text" style={{ padding: "5px", background: "#FFFFFF1F" }} name="" id="" /></div>
+        <input type="text" style={{ padding: "5px", margin: "10px", width: "394px", background: "#FFFFFF1F" }} name="" id="" />
+        <div className="flex">
+          <p>I agree to receive communications from <br /> Createx SEO Agency</p>
+          <p>Orci commodo, viverra orci mollis ut euismod?</p>
+          <button style={{ background: "#7772F1", padding: "10px", color: "white", borderRadius: "7px" }}>Try SEO toolkit</button>
+        </div>
+      </div>
+    </div>
+    <div className="flex justify-center items-center gap-24 p-16">
+      <div className="w-1/3">
+        <h1 className="font-sans font-semibold text-3xl pb-20">Freequently Ask Questions</h1>
+        <p className="font-sans font-semibold text-1xl" >Aliquet lectus urna viverra in odio?</p>
+        <p>Adipiscing nunc arcu enim elit mattis eu placerat proin. Imperdiet elementum faucibus dignissim purus. Fusce parturient diam magna ullamcorper morbi semper massa ac facilisis.</p>
+        <p className="font-sans font-semibold text-1xl">Orci commodo, viverra orci mollis ut euismod?</p>
+        <p className="font-sans font-semibold text-1xl">Orci commodo, viverra orci mollis ut euismod?</p>
+        <p className="font-sans font-semibold text-1xl">Orci commodo, viverra orci mollis ut euismod?</p>
+        <button style={{ background: "#7772F1", padding: "10px", color: "white", borderRadius: "7px" }}>Try SEO toolkit</button>
+      </div>
+      <img src="/src/pages/home/homeImg/illustration (17).png" alt="" />
+    </div>
+    <div className="bg-gray-200">
+      <div className="flex justify-between p-10"><h1 className="font-bold text-4xl">Read our clients' case studies</h1><img src="/src/pages/home/homeImg/hover.png" alt="" /></div>
+      <div className="leading-6 flex justify-between p-8">
+        <div className="text-start w-96 p-12 " style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+        <div className="text-start w-96 p-12 " style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+        <div className="text-start w-96 p-12 " style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+      </div>
+      <div className="flex items-center gap-16 justify-center">
+        <h1 className="font-serif font-semibold text-2xl">Explore more case studies</h1> 
+        <button className="w-2/12 p-4 text-white rounded-r bg-purple-400">View all case studies</button>
+      </div>
+    </div>
+    <div>
+      <h1 className="text-center">Our benefits</h1>
+      <div className="flex items-center justify-center gap-7">
+        <div>
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+        </div>
+        <img src="/src/pages/home/homeImg/illustration (18).png" alt="" />
+        <div className="">
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+            <div className=" m-5 text-center w-60 h-40 p-4  leading-10" style={{border:"1px solid gray"}}>
+              <img className="m-auto" src="/src/pages/home/homeImg/Group.png" alt="" />
+                <h1>Social Advertising Experts</h1>
+                <p className="text-xs">Culpa nostrud commodo ea consequat aliquip reprehenderit. Veniam velit nostrud aliquip sunt.</p>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1 className="font-semibold text-4xl text-center">Flexible pricing plans</h1>
+      <div className="text-center m-10">
+      <button className="w-40 p-3 text-white rounded-r bg-purple-400">Monthly </button>
+        <button style={{border:"1px solid purple", color:"purple",padding:"10px"}}>Yearly -12% Off</button>
+      </div>
+      <div className="flex justify-center gap-20" >
+        <div className="p-10" style={{border:"1px solid gray",width:"300px",textAlign:"center"}}>
+          <h1 className="text-3xl font-semibold p-3">Basic</h1>
+          <p className="text-4xl font-semibold p-3 text-purple-500">24$</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p p-3>✅Advanced Analytics</p>
+          <button style={{border:"1px solid purple",color:"purple",padding:"10px"}}>Choose plan</button>
+        </div>
+        <div className="p-10" style={{border:"1px solid gray",width:"300px",textAlign:"center"}}>
+          <h1 className="text-3xl font-semibold p-3">Basic</h1>
+          <p className="text-4xl font-semibold p-3 text-purple-500">24$</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p p-3>✅Advanced Analytics</p>
+          <button style={{border:"1px solid purple",color:"purple",padding:"10px"}}>Choose plan</button>
+        </div>
+        <div className="p-10" style={{border:"1px solid gray",width:"300px",textAlign:"center"}}>
+          <h1 className="text-3xl font-semibold p-3">Basic</h1>
+          <p className="text-4xl font-semibold p-3 text-purple-500">24$</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p className="p-1">✅Advanced Analytics</p>
+          <p p-3>✅Advanced Analytics</p>
+          <button style={{border:"1px solid purple",color:"purple",padding:"10px"}}>Choose plan</button>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1 className="text-center text-5xl p-5 font-semibold">What our clients say</h1>
+      <div className="font-serif text-gray-400 p-8 m-auto"  style={{width:"660px",}}>
+        <p className="text-start text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, amet lectus quam viverra mus lobortis fermentum amet, eu. Pulvinar eu sed purus facilisi. Vitae id turpis tempus ornare turpis quis non. Congue tortor in euismod vulputate etiam eros. Pulvinar neque pharetra arcu diam maecenas diam integer in.</p>
+        <div className="flex pt-5 items-center" >
+          <img src="/src/pages/home/homeImg/image (21).png" alt="" />
+          <div>
+            <h2 className="text-black">Courtney Alexander</h2>
+            <p>Position, Company name</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="flex justify-center gap-20">
+      <div className="w-56">
+        <h1 className="font-semibold text-3xl p-3 ">Latest news</h1>
+        <div className="p-3">
+        <p>Check more posts in our blog for featured news and advertising insights</p>
+        </div>
+        <button className="w-2/11 p-4 m-3 text-white rounded-r bg-purple-400">Go to blog</button>
+
+      </div>
+      <div className="text-gray-500 w-64">
+        <img src="/src/pages/home/homeImg/pic.png" alt="" />
+        <p>Marketing | July 2, 2020 | By Diane Mccoy</p>
+        <h1 className="text-black">The Basics of Blogging Search Engine Optimization.</h1>
+        <p className="text-justify">Mauris tincidunt sollicitudin tristique odio eget volutpat. Fringilla viverra amet, mi interdum blandit...</p>
+      </div>
+      <div className="text-gray-500 w-64">
+        <img src="/src/pages/home/homeImg/pic.png" alt="" />
+        <p>Marketing | July 2, 2020 | By Diane Mccoy</p>
+        <h1 className="text-black">The Basics of Blogging Search Engine Optimization.</h1>
+        <p className="text-justify">Mauris tincidunt sollicitudin tristique odio eget volutpat. Fringilla viverra amet, mi interdum blandit...</p>
+      </div>
+    </div>
   </>
 }
--- src/pages/service/service.jsx
+++ src/pages/service/service.jsx
@@ -1,5 +1,151 @@
 export default function Service(){
   return <>
-    <h1> Service</h1>
+    <div className="" style={{textAlign:"center",background: "radial-gradient(83.15% 154.4% at 10% 13.52%, #1E212C 0%, #232441 56.38%, #1E212C 100%)"}}>
+        
+        <div className="m-auto" style={{width:"650px"}}>
+          <h1 className="text-white text-5xl">Our services</h1>
+        <p className="p-5 text-gray-500">We offer more than services. Our agency provides solutions to your top-priority concerns, involving customer targeting, email performance and sales growth. If there is no existing solution readily available, we create one, specifically to suit your business.</p>
+      <div className="flex w-56 m-auto items-center">
+        <button style={{border:"1px solid white",padding:"10px",color:"white"}}>Start now</button>
+        <div className="text-start m-2">
+          <p>Call us</p>
+          <p>(405) 555-0128</p>
+        </div>
+      </div>
+          </div>
+          
+    </div>
+    <div className="flex items-center justify-center">
+      <div className="w-2/5">
+        <h1 className="text-3xl font-semibold">Social Media Marketing</h1>
+        <p className="pt-5 pb-5">Eleifend orci massa et facilisis orci feugiat ac. Congue in ut lacus, turpis accumsan gravida. Aliquet mattis dignissim massa sociis a, id bibendum. Egestas lorem eros, mi cras amet, vel eget molestie. Turpis enim arcu, amet donec massa pellentesque nulla duis. Arcu dictum metus sed purus senectus faucibus eget elementum pretium.         </p>
+        <div><button className="" style={{color:"blue",border:"1px solid blue",padding:"10px",margin:"6px"}}>Learn more</button><button style={{background: "rgba(119, 114, 241, 1)",padding:"10px",color:"white",borderRadius:"4px"}}>Try SEO toolkit</button></div>
+      </div>
+      <img src="/src/pages/services/serviceImg/illustration (19).png" alt="" />
+    </div>
+    <div className="flex items-center justify-center">
+    <img src="/src/pages/services/serviceImg/illustration (20).png" alt="" />
+
+      <div className="w-2/5">
+        <h1 className="text-3xl font-semibold">Search Engine Optimization</h1>
+        <p className="pt-5 pb-5">Eleifend orci massa et facilisis orci feugiat ac. Congue in ut lacus, turpis accumsan gravida. Aliquet mattis dignissim massa sociis a, id bibendum. Egestas lorem eros, mi cras amet, vel eget molestie. Turpis enim arcu, amet donec massa pellentesque nulla duis. Arcu dictum metus sed purus senectus faucibus eget elementum pretium.         </p>
+        <div><button className="" style={{color:"blue",border:"1px solid blue",padding:"10px",margin:"6px"}}>Learn more</button><button style={{background: "rgba(119, 114, 241, 1)",padding:"10px",color:"white",borderRadius:"4px"}}>Try SEO toolkit</button></div>
+      </div>
+    </div>
+    <div className="flex items-center justify-center">
+      <div className="w-2/5">
+        <h1 className="text-3xl font-semibold">Social Media Marketing</h1>
+        <p className="pt-5 pb-5">Eleifend orci massa et facilisis gestas lorem eros, mi cras amet, vel eget molestie. Turpis enim arcu, amet donec massa pellentesque nulla duis. Arcu dictum metus sed purus senectus faucibus eget elementum pretium.         </p>
+        <div><button className="" style={{color:"blue",border:"1px solid blue",padding:"10px",margin:"6px"}}>Learn more</button><button style={{background: "rgba(119, 114, 241, 1)",padding:"10px",color:"white",borderRadius:"4px"}}>Try SEO toolkit</button></div>
+      </div>
+      <img src="/src/pages/services/serviceImg/illustration (21).png" alt="" />
+    </div>
+    <div className="flex items-center justify-center">
+      <div className="w-2/5">
+        <h1 className="text-3xl font-semibold">Payed Traffic Management</h1>
+        <p className="pt-5 pb-5">Eleifend orci massa et facilisis gestas lorem eros, mi cras amet, vel eget molestie. Turpis enim arcu, amet donec massa pellentesque nulla duis. Arcu dictum metus sed purus senectus faucibus eget elementum pretium.         </p>
+        <div><button className="" style={{color:"blue",border:"1px solid blue",padding:"10px",margin:"6px"}}>Learn more</button><button style={{background: "rgba(119, 114, 241, 1)",padding:"10px",color:"white",borderRadius:"4px"}}>Try SEO toolkit</button></div>
+      </div>
+      <img src="/src/pages/services/serviceImg/illustration (22).png" alt="" />
+    </div>
+    <div className="bg-gray-900 flex text-gray-400 items-center justify-center gap-24 p-16" >
+      <img src="/src/pages/home/homeImg/illustration (16).png" alt="" />
+      <div className="">
+        <h1>Get a Free SEO Analysis!</h1>
+        <div><input type="text" style={{ padding: "5px", margin: "10px", background: "#FFFFFF1F" }} name="" id="" /><input type="text" style={{ padding: "5px", background: "#FFFFFF1F" }} name="" id="" /></div>
+        <input type="text" style={{ padding: "5px", margin: "10px", width: "394px", background: "#FFFFFF1F" }} name="" id="" />
+        <div className="flex">
+          <p>I agree to receive communications from <br /> Createx SEO Agency</p>
+          <button style={{ background: "#7772F1", padding: "10px", color: "white", borderRadius: "7px" }}>Try SEO toolkit</button>
+        </div>
+      </div>
+    </div>
+    <div className="p-20">
+      <h1 className="text-center text-3xl font-semibold p-10">Markets in which we operate</h1>
+      <div className="flex justify-center gap-14">
+        <div className="text-center w-80">
+            <img className="m-auto" src="/src/pages/services/serviceImg/ic-cart.png" alt="" />
+            <h1>E-commerce Websites</h1>
+            <p>Aenean urna dictum adipiscing nec, cras quisque. Nunc in mauris. </p>
+
+        </div>
+        <div className="text-center w-80">
+            <img className="m-auto" src="/src/pages/services/serviceImg/ic-cart.png" alt="" />
+            <h1>E-commerce Websites</h1>
+            <p>Aenean urna dictum adipiscing nec, cras quisque. Nunc in mauris. </p>
+
+        </div>
+        <div className="text-center w-80">
+            <img className="m-auto" src="/src/pages/services/serviceImg/ic-cart.png" alt="" />
+            <h1>E-commerce Websites</h1>
+            <p>Aenean urna dictum adipiscing nec, cras quisque. Nunc in mauris. </p>
+
+        </div>
+        <div className="text-center w-80">
+            <img className="m-auto" src="/src/pages/services/serviceImg/ic-cart.png" alt="" />
+            <h1>E-commerce Websites</h1>
+            <p>Aenean urna dictum adipiscing nec, cras quisque. Nunc in mauris. </p>
+
+        </div>
+      </div>
+    </div>
+    <div className="bg-gray-200">
+      <div className="flex justify-between p-10"><h1 className="font-bold text-4xl">Read our clients' case studies</h1><img src="/src/pages/home/homeImg/hover.png" alt="" /></div>
+      <div className="leading-6 flex justify-between p-8">
+        <div className="text-start w-96 p-12 bg-white " style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+        <div className="text-start w-96 p-12  bg-white " style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+        <div className="text-start w-96 p-12   bg-white" style={{border: "1px solid #E5E8ED"}}>
+          <img src="/src/pages/home/homeImg/client-logo (1).png" alt="" />
+          <div>
+              <p>Charity organisation</p>
+          <p>Createx SEO Agency helped National Inc. increase their MQL to SQL conversion rate by 300%.</p>
+          </div>
+        
+        <img className="pt-10" src="/src/pages/home/homeImg/features (3).png" alt="" />
+        </div>
+      </div>
+      <div className="flex items-center gap-16 justify-center">
+        <h1 className="font-serif font-semibold text-2xl">Explore more case studies</h1> 
+        <button className="w-2/12 p-4 text-white rounded-r bg-purple-400">View all case studies</button>
+      </div>
+    </div>
+    <div className="flex justify-center gap-20 p-10">
+      <div className="w-56">
+        <h1 className="font-semibold text-3xl p-3 ">Latest news</h1>
+        <div className="p-3">
+       <p>Check more posts in our blog for featured news and advertising insights</p>
+        </div>
+        <button className="w-2/11 p-4 m-3 text-white rounded-r bg-purple-400">Go to blog</button>
+
+      </div>
+      <div className="text-gray-500 w-64">
+        <img src="/src/pages/home/homeImg/pic.png" alt="" />
+        <p>Marketing | July 2, 2020 | By Diane Mccoy</p>
+        <h1 className="text-black">The Basics of Blogging Search Engine Optimization.</h1>
+        <p className="text-justify">Mauris tincidunt sollicitudin tristique odio eget volutpat. Fringilla viverra amet, mi interdum blandit...</p>
+      </div>
+      <div className="text-gray-500 w-64">
+        <img src="/src/pages/home/homeImg/pic.png" alt="" />
+        <p>Marketing | July 2, 2020 | By Diane Mccoy</p>
+        <h1 className="text-black">The Basics of Blogging Search Engine Optimization.</h1>
+        <p className="text-justify">Mauris tincidunt sollicitudin tristique odio eget volutpat. Fringilla viverra amet, mi interdum blandit...</p>
+      </div>
+    </div>
   </>
 }
